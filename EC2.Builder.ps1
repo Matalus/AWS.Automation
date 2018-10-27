b87d576ff73cb72fe00ae87adb205ea31c4978df
matth <matt.hamende@gmail.com>
2018-10-27 15:37:10 -0700

    initial commit

---
+++ EC2.Builder.ps1
@@ -0,0 +1,67 @@
+#Set Root Dir
+$RunDir = Split-path -Parent $MyInvocation.MyCommand.Definition
+
+#Load Config
+$Config = (Get-Content $RunDir\aws.json) -join "`n" | ConvertFrom-Json
+
+#Load AWS PowerShell
+if(!(Get-Module AWSPowerShell)){
+  Import-Module AWSPowerShell
+}
+
+  Import-Module "$RunDir\Modules\functions" -DisableNameChecking
+  Log "Functions Loaded" "Yellow"
+
+Log "Getting default EC2 Image..."
+[array]$EC2Image = Get-EC2ImageByName -Name $Config.DefaultImageName
+if($EC2Image.count -ge 1){
+  Log "Found $($EC2Image.count) Image/s" "Cyan"
+  $EC2Image | ForEach-Object { 
+    "  +Architecture: $($_.Architecture) `n  +ImageId: $($_.ImageId) `n  +Description: $($_.Description)"
+  }
+}
+
+Log "Getting EC2 Instances...";""
+
+ForEach($Instance in $Config.Instances){
+  Log "Valdating Config : $($Instance.Name)"
+  $InstanceExist = $null
+
+  While($null -eq $InstanceExist){
+    $InstanceExist = Get-EC2Instance -Filter @{Name="tag:Name";Values=$Instance.Name}
+    if($InstanceExist){ 
+      Log "Instance $($Instance.Name) Already Exists" "Cyan"
+    }else{
+      Log "Instance $($Instance.Name) Does not Exist, Creating..." "Yellow"
+      Log "Getting Image..."
+      $Image = Get-EC2ImageByName -Name $Instance.Image
+
+      $params = @{
+        ImageId = $Image.ImageId
+        MinCount = 1
+        MaxCount = 1
+        InstanceType = $Instance.Type
+      }
+      $InstanceExist = New-EC2Instance @params
+      $Tag = New-EC2Tag -Resource $InstanceExist.Instances[0].InstanceId -Tag @{Key="Name";Value=$Instance.Name}
+      $InstanceExist = Get-EC2Instance -Filter @{Name="tag:Name";Values=$Instance.Name}
+
+    }
+  }
+  $InstanceObj = [PSCustomObject]@{
+    Name = ($InstanceExist.Instances.Tag | Where-Object {$_.Key -eq "Name"})[0].Value 
+    Details = $InstanceExist.Instances[0] 
+    status = $InstanceExist.Instances[0].State.Name.Value  
+  }
+  
+ @"
+  Name: $(if($InstanceObj.Name){$instanceObj.Name}else{'null'}) 
+    + Platform: $($instanceObj.Details.Platform) 
+    + InstanceType: $($instanceObj.Details.InstanceType)
+    + Public IP: $($InstanceObj.Details.PublicIpAddress)
+    + Status: $($InstanceObj.status)
+"@
+""
+}
+
+
---
+++ Modules/functions/functions.psm1
@@ -0,0 +1,10 @@
+#Generic Logging function --
+Function Log($message, $color) {
+  if ($color) {
+      Write-Host -ForegroundColor $color "$(Get-Date -Format u) | $message"
+  }
+  else {
+      "$(Get-Date -Format u) | $message"
+  }
+}
+Export-ModuleMember Log
